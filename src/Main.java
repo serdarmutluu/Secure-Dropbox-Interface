5983f56e2bcc7e2fdecba7d678ac473d646d7dc6
serdarmutlu <serdarmutlu799@gmail.com>
2022-03-30 16:11:24 +0300

    login added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxAuthFinish;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
@@ -12,7 +14,7 @@ import java.io.InputStream;
 public class Main {
     public static void main(String[] args) throws DbxException, IOException {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/").build();
-        String token = "sl.BEr8SM-PPnhbhxhhnSjyJ8689GMHorV1ElEgrZV3Vw2ZgMSnCxEgful3aueL7MEyf0tt2glo3n0gIfNaVIngg21jp7_3Lz6pNz6pAtUFvkD-7EHkK7sk-mB44kM3mGZWxX0o7h7ObUgg";
+        /*String token = "sl.BExv5gV21qKK_5it4q6IWl30xCTtmRpy7RmVdJdGyBQalzfsclgX-m2TnzSJBhVRIDdj8PLH2tzy4KcY6iFSBEU6HC7X2DMa3Diqwq-5h5nLtWObqiZIQRhOzYGLz5z_Eozkug7hAXvw";
         DbxClientV2 dbx = new DbxClientV2(config, token);
 
         FullAccount account = null;
@@ -22,7 +24,7 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(account.getName().getDisplayName());
-
+        System.out.println(dbx.users());
         ListFolderResult result = dbx.files().listFolder("/dedem");
         while (true) {
             for (Metadata metadata : result.getEntries()) {
@@ -40,7 +42,17 @@ public class Main {
         try (InputStream in = new FileInputStream("test.txt")) {
             FileMetadata metadata = dbx.files().uploadBuilder("/dedem/test.txt")
                     .uploadAndFinish(in);
-        }
+        }*/
+        DbxAppInfo appInfo  = new DbxAppInfo("aed4jqlgklt5iek","4okiktgmoqgiu50");
+        DbxAuthFinish authFinish = null;
+        authFinish = new PkceAuthorize().authorize(appInfo);
+        System.out.println("Authorization complete.");
+        System.out.println("- User ID: " + authFinish.getUserId());
+        System.out.println("- Account ID: " + authFinish.getAccountId());
+        System.out.println("- Access Token: " + authFinish.getAccessToken());
+        System.out.println("- Expires At: " + authFinish.getExpiresAt());
+        System.out.println("- Refresh Token: " + authFinish.getRefreshToken());
+        System.out.println("- Scope: " + authFinish.getScope());
 
     }
 }
