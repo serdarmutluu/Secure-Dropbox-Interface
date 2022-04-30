bb340619e159ab46787d86fde4f1fb08c9aa0418
serdarmutlu <serdarmutlu799@gmail.com>
2022-04-30 18:58:32 +0300

    added file listing class

---
+++ src/FileLister.java
@@ -0,0 +1,25 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FolderMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+
+public class FileLister {
+    static void list(DbxClientV2 dbx,String path) throws DbxException {
+        ListFolderResult result = dbx.files().listFolder(path);
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                if(metadata instanceof FolderMetadata){
+                    list(dbx,metadata.getPathLower());
+                }
+                System.out.println(metadata.getPathLower());
+            }
+
+            if (!result.getHasMore()) {
+                return;
+            }
+
+            result = dbx.files().listFolderContinue(result.getCursor());
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,24 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.FolderMetadata;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.users.FullAccount;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.DirectoryStream;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws DbxException, IOException {
+
+
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/").build();
-        /*String token = "sl.BExv5gV21qKK_5it4q6IWl30xCTtmRpy7RmVdJdGyBQalzfsclgX-m2TnzSJBhVRIDdj8PLH2tzy4KcY6iFSBEU6HC7X2DMa3Diqwq-5h5nLtWObqiZIQRhOzYGLz5z_Eozkug7hAXvw";
-        DbxClientV2 dbx = new DbxClientV2(config, token);
-
-        FullAccount account = null;
-        try {
-            account = dbx.users().getCurrentAccount();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(account.getName().getDisplayName());
-        System.out.println(dbx.users());
-        ListFolderResult result = dbx.files().listFolder("/dedem");
-        while (true) {
-            for (Metadata metadata : result.getEntries()) {
-                System.out.println(metadata.getPathLower());
-            }
-
-            if (!result.getHasMore()) {
-                break;
-            }
-
-            result = dbx.files().listFolderContinue(result.getCursor());
-        }
-
-        // Upload "test.txt" to Dropbox
-        try (InputStream in = new FileInputStream("test.txt")) {
-            FileMetadata metadata = dbx.files().uploadBuilder("/dedem/test.txt")
-                    .uploadAndFinish(in);
-        }*/
         DbxAppInfo appInfo  = new DbxAppInfo("aed4jqlgklt5iek","4okiktgmoqgiu50");
         DbxAuthFinish authFinish = null;
         authFinish = new PkceAuthorize().authorize(appInfo);
@@ -53,6 +32,25 @@ public class Main {
         System.out.println("- Expires At: " + authFinish.getExpiresAt());
         System.out.println("- Refresh Token: " + authFinish.getRefreshToken());
         System.out.println("- Scope: " + authFinish.getScope());
+        String token = authFinish.getAccessToken();
+        DbxClientV2 dbx = new DbxClientV2(config, token);
+
+        FullAccount account = null;
+        try {
+            account = dbx.users().getCurrentAccount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(account.getName().getDisplayName());
+        System.out.println(dbx.users());
+        FileLister.list(dbx,"");
+
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("test.txt")) {
+            FileMetadata metadata = dbx.files().uploadBuilder("/dedem/test.txt")
+                    .uploadAndFinish(in);
+        }
+
 
     }
 }
